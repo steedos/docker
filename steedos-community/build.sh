0d29f713e431dd76404e926b55dc91a4b95a2a5a
庄建国 <9340100@qq.com>
2022-06-15 15:51:26 +0000

    steedos/steedos-community:2022.6.16

--- steedos-community/build.sh
+++ steedos-community/build.sh
@@ -1,9 +1,5 @@
 docker build --tag steedos/steedos-community:latest ./2022
 
-docker tag steedos/steedos-community steedos/steedos-community:2022.6.15
+docker tag steedos/steedos-community steedos/steedos-community:2022.6.16
 docker tag steedos/steedos-community steedos/steedos-community:2022.6
 docker tag steedos/steedos-community steedos/steedos-community:2022
-
-docker push steedos/steedos-community:latest
-docker push steedos/steedos-community:2022.6.15
-docker push steedos/steedos-community:2022.6
---
+++ steedos-community/push.sh
@@ -0,0 +1,4 @@
+
+docker push steedos/steedos-community:latest
+docker push steedos/steedos-community:2022.6.16
+docker push steedos/steedos-community:2022.6
