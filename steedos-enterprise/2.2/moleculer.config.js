25940b04c7bbbdff906c79a4e99bca8448ee247d
庄建国 <9340100@qq.com>
2022-06-22 10:26:58 +0000

    fix template

--- steedos-enterprise/2.2/moleculer.config.js
+++ steedos-enterprise/2.2/moleculer.config.js
@@ -30,7 +30,7 @@ const LokiTransport = require("winston-loki");
  */
 module.exports = {
 	// Namespace of nodes to segment your nodes on the same network.
-	namespace: "steedos-project-template",
+	namespace: "steedos",
 	// Unique node identifier. Must be unique in a namespace.
 	nodeID: process.env.NODEID,
 	// Custom metadata store. Store here what you want. Accessing: `this.broker.metadata`
