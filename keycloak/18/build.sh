1e5339d541368fd9ee05dd4b032e1c9c64d1d290
庄建国 <9340100@qq.com>
2022-06-25 03:37:19 +0000

    KEYCLOAK_VERSION=18.0.2

--- keycloak/18/build.sh
+++ keycloak/18/build.sh
@@ -1,8 +1,14 @@
+export KEYCLOAK_VERSION=18.0.2
+
+echo "#########################################################################"
+echo "keycloak version: ${KEYCLOAK_VERSION}"
+echo "#########################################################################"
 
 docker build --rm --no-cache \
     --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")" \
     --build-arg TAG_SUFFIX=default \
+    --build-arg KEYCLOAK_VERSION=${KEYCLOAK_VERSION} \
     --file Dockerfile \
     --tag steedos/keycloak .
 
-docker tag steedos/keycloak steedos/keycloak:18.0.1
+docker tag steedos/keycloak steedos/keycloak:${KEYCLOAK_VERSION}
