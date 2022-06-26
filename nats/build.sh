0502d91b0ba96d239e2517add41a64d8f4a1b178
庄建国 <9340100@qq.com>
2022-06-26 04:43:31 +0000

    add nats password

--- nats/build.sh
+++ nats/build.sh
@@ -1,1 +1,8 @@
-docker build --tag steedos/nats:2.8.4 ./2.8
+export NATS_VERSION=2.8.4
+
+docker build  --rm --no-cache \
+    --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")" \
+    --build-arg NATS_VERSION=${NATS_VERSION} \
+    --tag steedos/nats ./2.8
+
+docker tag steedos/nats steedos/nats:${NATS_VERSION}
