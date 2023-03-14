eda2c2dc799d399d8b6a0dd94d6b6e5643ad9efb
庄建国 <9340100@qq.com>
2023-03-14 08:29:50 +0000

    vscode server

---
+++ steedos-vscode-server/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+export VSCODE_SERVER_VERSION=1.76.1
+
+echo "#########################################################################"
+echo "steedos vscode server version: ${VSCODE_SERVER_VERSION}"
+echo "#########################################################################"
+
+docker build --rm --no-cache \
+    --build-arg ARCH=amd64 \
+    --build-arg NODE_VERSION=14 \
+    --build-arg VSCODE_SERVER_VERSION=${VSCODE_SERVER_VERSION} \
+    --build-arg OS=alpine3.12 \
+    --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")" \
+    --build-arg TAG_SUFFIX=default \
+    --file Dockerfile \
+    --tag steedos/vscode-server .
+
+docker tag steedos/vscode-server steedos/vscode-server:${VSCODE_SERVER_VERSION}
